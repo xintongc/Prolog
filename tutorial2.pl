79e3724e3ee10b9182fe39350ea4e11976f73977
Claire (Xintong) <xintongc@gmail.com>
2017-10-24 23:00:13 -0400

    Update tutorial2.pl

--- tutorial2.pl
+++ tutorial2.pl
@@ -48,19 +48,16 @@ _ is anonymous  variable
 
 
 append_list([],L,L).
- append_lis([H|T],L,[H|R]):-append_list(T,L,R).
+append_lis([H|T],L,[H|R]):-append_list(T,L,R).
 
-
-trace.
 
 
 prefix(P,L) means P is the prefix of L. ex. 
 
 
-
-
 prefix(P,L):-append(p,_,L).
 suffix(S,L):-append(_,S,L).
+
 
 S is sublist of L (L1,L2,newlist)
 sublist(S,L):-append(_,S,P),append(P,_,L).
@@ -95,3 +92,29 @@ gcd(X,Y,G) :- G is the greatest common divisor of X and Y.
 
 gcd(X,0,X) :- X > 0.
 gcd(X,Y,G) :- Y > 0, Z is X mod Y, gcd(Y,Z,G).
+
+Write down a Prolog procedure third-to-last to find the third-to-last element from a list
+third-to-last(X,[X,Y,_]).
+third-to-last(X,[Y|Ys]) :- third-to-last(X,Ys).
+
+
+range(I, K, L) :- I <= K, and L is the list containing all consecutive integers from I to K
+range (1,4,X) =>   X=[1,2,3,4]
+
+range(I,I,[I]).
+range(I,K,[I|L]) :- I < K, I1 is I + 1, range(I1,K,L).
+
+
+Contain(X, [X|_]).
+Contain(X, [_|T]) :- contain(X,T).
+
+
+Takes two lists as arguments, and succeeds if the first list is twice the size of the second list.
+a2b([],[]).
+a2b( [_,_|Ta], [_|Tb] ) :- a2b(Ta, Tb).
+
+
+
+Write a code to determine if a list contains negative number or not.
+neg([X|Y]):- X<0.
+neg([X|Y]):- X>=0, neg(Y).
